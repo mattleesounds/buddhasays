06ca3510018afca206c88b74f59ec98cf28b4a2e
Matt Lee <mattleesounds@gmail.com>
2024-03-04 11:35:52 -0600

    updated api calls

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from "openai";
+/* import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -22,3 +22,48 @@ function generatePrompt(prompt) {
   Follower: ${prompt}
   Siddhartha: `;
 }
+ */
+
+/* import { Configuration, OpenAIApi, OpenAI } from "openai"; */
+import OpenAI from "openai"
+
+/* const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+}); */
+/* const openai = new OpenAIApi(configuration); */
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export default async function (req, res) {
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "You are Siddhartha Gautama (the Buddha) giving advice to one of your followers. The year is 400 B.C.E.",
+        },
+        {
+          role: "user",
+          content: req.body.prompt,
+        },
+      ],
+      
+    });
+
+    //console.log(completion.choices[0].message.content)
+
+    // Extract the text from the latest assistant message
+    const result = response.choices[0].message.content;
+
+    res.status(200).json({ result });
+  } catch (error) {
+    console.error("Error with OpenAI request:", error.response ? error.response.data : error);
+    res.status(500).json({
+      error: "Internal Server Error",
+      details: error.response ? error.response.data : "No additional error details",
+    });
+  }
+}
+
