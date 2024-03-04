ce5c504e62380affcce1e0bc22afb7a217e494c7
Matt Lee <mattleesounds@gmail.com>
2024-03-04 11:39:31 -0600

    u

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,36 +1,5 @@
-/* import { Configuration, OpenAIApi } from "openai";
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-export default async function (req, res) {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: generatePrompt(req.body.prompt),
-    temperature: 0.6,
-    max_tokens: 1000,
-  });
-  res.status(200).json({ result: completion.data.choices[0].text });
-}
-
-function generatePrompt(prompt) {
-  return `Siddhartha Gautama, also known as the Buddha, 
-  is giving advice to one of his followers. The year is 400 B.C.E.
-
-  Follower: ${prompt}
-  Siddhartha: `;
-}
- */
-
-/* import { Configuration, OpenAIApi, OpenAI } from "openai"; */
 import OpenAI from "openai"
 
-/* const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-}); */
-/* const openai = new OpenAIApi(configuration); */
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -48,13 +17,11 @@ export default async function (req, res) {
           role: "user",
           content: req.body.prompt,
         },
+        
       ],
+      max_tokens: 200
       
     });
-
-    //console.log(completion.choices[0].message.content)
-
-    // Extract the text from the latest assistant message
     const result = response.choices[0].message.content;
 
     res.status(200).json({ result });
